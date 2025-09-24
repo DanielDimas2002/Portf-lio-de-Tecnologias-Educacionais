c308abc2e0c926c786b39f15624ef72c825868f4
Daniel Dimas de Souza <101479145+DanielDimas2002@users.noreply.github.com>
2025-09-24 11:38:47 -0300

    Inicio do Projeto e Limpeza

---
+++ portfolio-tecnologias/src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Portfólio de Tecnologias Educacionais</h1>
+      <p>Em desenvolvimento...</p>
+    </div>
+  );
+}
+
+export default App;
